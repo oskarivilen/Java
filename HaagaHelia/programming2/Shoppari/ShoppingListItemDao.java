4896766e53c6d60ccbdfbf5bedbe2c5306bf1efb
oskarivilen <54893168+oskarivilen@users.noreply.github.com>
2020-04-23 12:14:04 +0300

    Create ShoppingListItemDao.java

---
+++ HaagaHelia/programming2/Shoppari/ShoppingListItemDao.java
@@ -0,0 +1,16 @@
+package shoppari;
+
+import java.util.List;
+
+import shoppari.ShoppingListItem;
+
+public interface ShoppingListItemDao {
+
+    public List<ShoppingListItem> getAllItems();
+
+    public ShoppingListItem getItem(long id);
+
+    public boolean addItem(ShoppingListItem newItem);
+
+    public boolean removeItem(ShoppingListItem item);
+}
